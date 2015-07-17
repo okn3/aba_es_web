f617eb5f4b545674324f39534dcfd139f33c9d58
甘利聡 <swkaen@amarisatoshi-no-MacBook-Pro.local>
2015-07-17 20:54:23 +0900

    検知結果をCSVに書き込む

--- get_data.py
+++ get_data.py
@@ -12,53 +12,67 @@ import serial
 import time
 import datetime
 import sys
+detect_id=0;
+def send_data(de_id):
+    global detect_id
+    detect_id = de_id
 
-unixTime_new = 0.0
-unixTime_old = 0.0
-rowCSV = 0
-baudrate = 19200
 
-argv = sys.argv
-if len(argv) == 3:
-    baudrate = argv[2]
-else:
-    print("usage: $python get_data.py <username>")
-    print("usage: $python get_data.py <username> <baudrate>")
-    exit()
+def get_data():
+    unixTime_new = 0.0
+    unixTime_old = 0.0
+    rowCSV = 0
+    baudrate = 19200
 
-# mbed のポートオープン
-# (unix系のOS)
-# コマンド $ls /dev/tty.* で mbed の名前を見れるので、ボーレートは適宜変えること
-# Windows未対応
-com = serial.Serial('/dev/tty.usbmodem1422', baudrate)
-day_info = datetime.datetime.today()
-# 元プログラム
-#csv_name = "lifilm_" + day_info.year + '_' + day_info.month + '_' + day_info.day + '_' + day_info.minute + '_' + day_info.second + '_' + argv[1] + ".csv"
-# python2対応
-csv_name = "lifilm" + '_' + str(day_info.year) + '_' + str(day_info.month) + '_' + str(day_info.day) + '_' + str(day_info.minute) + '_' + str(day_info.second) + '_' + str(argv[1]) + ".csv"
-while True:
-    time.sleep(5)
-    com.write('a')                      # データ送信要求 ('a' である必要性は無し)
-    time.sleep(5)
-    unixTime_new = int(time.time())     # unix秒を取得
+    argv = sys.argv
 
-    data = com.readline().split(',')
+    if len(argv) == 3:
+        baudrate = argv[2]
+    else:
+        print("usage: $python get_data.py <username>")
+        print("usage: $python get_data.py <username> <baudrate>")
+        exit()
 
-    if rowCSV != 0:
-        timeStamp = time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(unixTime_new))
-        dt = unixTime_new - unixTime_old
+    # mbed のポートオープン
+    # (unix系のOS)
+    # コマンド $ls /dev/tty.* で mbed の名前を見れるので、ボーレートは適宜変えること
+    # Windows未対応
+    com = serial.Serial('/dev/tty.usbmodem1422', baudrate)
+    day_info = datetime.datetime.today()
+    # 元プログラム
+    #csv_name = "lifilm_" + day_info.year + '_' + day_info.month + '_' + day_info.day + '_' + day_info.minute + '_' + day_info.second + '_' + argv[1] + ".csv"
+    # python2対応
+    csv_name = "lifilm" + '_' + str(day_info.year) + '_' + str(day_info.month) + '_' + str(day_info.day) + '_' + str(day_info.minute) + '_' + str(day_info.second) + '_' + str(argv[1]) + ".csv"
+    while True:
+        time.sleep(5)
+        com.write('a')                      # データ送信要求 ('a' である必要性は無し)
+        time.sleep(5)
+        unixTime_new = int(time.time())     # unix秒を取得
 
-        # csvへの書き込み行
-        row = timeStamp + ',' + str(dt)  + ',' + data[0] + ',' + data[1] + ',' + data[2] + ',' + data[11] + ',' + data[16] + '\n'
+        data = com.readline().split(',')
 
-        try:
-            fp1 = open(csv_name, 'a')
-            fp1.write(row)
-        except Eception as err:
-            print str(type(err))
-            print str(err)
-        finally:
-            fp1.close()
+        if rowCSV != 0:
+            timeStamp = time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(unixTime_new))
+            dt = unixTime_new - unixTime_old
 
-    rowCSV += 1
-    unixTime_old = unixTime_new
+            # csvへの書き込み行
+            row = timeStamp + ',' + str(dt)  + ',' + data[0] + ',' + data[1] + ',' + data[2] + ',' + data[11] + ',' + data[16] + ',' + str(detect_id) + '\n'
+
+            try:
+                fp1 = open(csv_name, 'a')
+                fp1.write(row)
+            except Eception as err:
+                print str(type(err))
+                print str(err)
+            finally:
+                fp1.close()
+
+        rowCSV += 1
+        unixTime_old = unixTime_new
+
+        if detect_id != 0:
+            send_data(0)
+
+        print "kitemasu"
+
+
--- index.py
+++ index.py
@@ -1,26 +1,36 @@
 # -*- coding: utf-8 -*-
 from bottle import route, run, template, static_file
+from get_data import send_data, get_data
+import threading
+def main():
+    @route('/test')
+    def test():
+        return "test"
 
-@route('/test')
-def test():
-    return "test"
+    @route('/')
+    def root():
+        return template('input',msg="")
 
-@route('/')
-def root():
-    return template('input',msg="")
+    @route('/<id:int>')
+    def detect(id):
 
-@route('/<id:int>')
-def detect(id):
+        #処理書く
+        
+        if id == 1:
+            msg="尿"
+            send_data(1)
+        elif id == 2:
+            msg="便"
+            send_data(2)
+        else:
+            msg="なし"
+            send_data(3)
+        return template('input',msg=msg)
 
-    #処理書く
-    
-    if id == 1:
-        msg="尿"
-    elif id == 2:
-        msg="便"
-    else:
-        msg="なし"
-    return template('input',msg=msg)
 
+    run(host='localhost', port=8080)
 
-run(host='localhost', port=8080)
+index_th = threading.Thread(target=main, name = "intdex_th")
+get_th = threading.Thread(target=get_data, name = "get_th")
+index_th.start()
+get_th.start()
